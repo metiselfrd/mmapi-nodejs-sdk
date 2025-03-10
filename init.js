b693cbef2934bd23db6050a4db6a71c3bb0cf9c7
metiselfrd <infor@metisel.com>
2025-03-10 15:38:06 +0000

    Update init.js with additional initialization details

---
+++ init.js
@@ -0,0 +1,35 @@
+/**
+ * MMAPI Node.js SDK dependency
+ */
+const mmapi = require('../lib/index');
+
+/**
+ * Set up and return MMAPI Node.js SDK environment.
+ */
+const consumerKey = process.env.CONSUMER_KEY;
+const consumerSecret = process.env.CONSUMER_SECRET;
+const apiKey = process.env.API_KEY;
+const securityOption = process.env.SECURITY_OPTION; // optional: DEVELOPMENT_LEVEL, STANDARD_LEVEL, ENHANCED_LEVEL
+const callbackUrl = process.env.CALLBACK_URL;
+let environment;
+
+// Use production environment if NODE_ENV is set to production; otherwise use sandbox.
+if (process.env.NODE_ENV === 'production') {
+  environment = new mmapi.core.LiveEnvironment(consumerKey, consumerSecret, apiKey, securityOption, callbackUrl);
+} else {
+  environment = new mmapi.core.SandboxEnvironment(consumerKey, consumerSecret, apiKey, securityOption, callbackUrl);
+}
+
+/**
+ * Returns MMAPI Node.js SDK HTTP client instance with environment.
+ * Use this instance to invoke MMAPI APIs.
+ */
+let client = new mmapi.core.MobileMoneyApiHttpClient(environment);
+
+console.log("Consumer Key : " + client.environment.consumerKey);
+console.log("Consumer Secret : " + client.environment.consumerSecret);
+console.log("API Key : " + client.environment.apiKey);
+console.log("Environment : " + client.environment.environment);
+console.log("Security Level : " + client.environment.securityOption);
+console.log("Callback URL : " + client.environment.callbackUrl);
+console.log("MMAPI URL : " + client.environment.baseUrl);
